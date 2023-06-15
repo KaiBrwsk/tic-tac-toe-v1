e49456af8092339daf763fe3c567e0a19633b149
KaiBrwsk <133599035+KaiBrwsk@users.noreply.github.com>
2023-06-15 17:03:54 +0200

    Update comments

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ running = True
 
 
 def place_computer_token():
+    # Generate random valid position
     computer_row = random.randint(0, 2)
     computer_column = random.randint(0, 2)
 
+    # Check if created position is empty
     if check_free(computer_row, computer_column):
+        # Place computer token at created position
         game_token[computer_row][computer_column] = COMPUTER_TOKEN
     else:
+        # Start again and create new random position
         place_computer_token()
 
 
@@ -46,8 +50,10 @@ def check_winner(token):
 def check_free(row, column):
     if row >= 0 and column >= 0:
         try:
+            # Check is desired position is empty
             if game_token[row][column] == " ":
                 return True
+        # Catch error when desired position is outside of game board
         except IndexError:
             return False
     else:
@@ -57,7 +63,8 @@ def check_free(row, column):
 def game():
     global rounds
 
-    # Place users token
+    # User turn
+    # Check if empty fields are available on game board
     if rounds < 9:
         # Ask user for position of token
         user_row = int(input("In which row do you want to place your token? (1/2/3): ")) - 1
@@ -65,10 +72,12 @@ def game():
 
         # Check if position is free
         if check_free(user_row, user_column):
+            # Place user token at desired position
             game_token[user_row][user_column] = USER_TOKEN
             rounds += 1
         else:
             print("Sorry, this is not possible. Please choose another location.\n")
+            # Ask user again for desired position
             game()
 
         # Show game board to user
@@ -80,13 +89,16 @@ def game():
         if check_winner(USER_TOKEN):
             print("You win.")
             return
+    # No empty fields = No winner
     else:
         print("No winner.")
         return
 
-    # Place computers token
+    # Computer turn
+    # Check if empty fields are available on game board
     if rounds < 9:
         print("The computer is placing a token. Please wait a moment.")
+        # Simulate computer taking time to place the token
         time.sleep(2)
         place_computer_token()
         rounds += 1
@@ -101,8 +113,9 @@ def game():
             print("The computer wins.")
             return
         else:
-            # Restart game loop
+            # Ask user for desired position
             game()
+    # No empty fields = No winner
     else:
         print("No winner.")
         return
@@ -120,6 +133,7 @@ def clear():
 
 def show_game_board():
     clear()
+    # Connect game board with tokens placed by user and computer
     game_board = (f"{game_token[0][0]} | {game_token[0][1]} | {game_token[0][2]}\n"
                   "---------\n"
                   f"{game_token[1][0]} | {game_token[1][1]} | {game_token[1][2]}\n"
@@ -130,8 +144,12 @@ def show_game_board():
 
 
 while running:
+    # Ask user if they want to play game
     user_play_again = input("Would you like to play? (yes/no): ").lower()
+
+    # Check user input
     if user_play_again == "yes":
+        # Reset and prepare game variables
         game_token = [
             [" ", " ", " "],
             [" ", " ", " "],
@@ -140,8 +158,11 @@ while running:
         show_game_board()
         rounds = 0
         game()
-    else:
+    elif user_play_again == "no":
+        # Stop the loop and end game.
         clear()
         print("See you next time.")
         running = False
+    else:
+        print("Sorry, I did not understand your wish.\n")
 
